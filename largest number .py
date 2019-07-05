4257b4c8263c77ec19c3eb9c7ec10d758c72a3fd
sandhiyasajiv <52270651+sandhiyasajiv@users.noreply.github.com>
2019-07-05 14:05:13 +0530

    Create largest number .py

---
+++ largest number .py
@@ -0,0 +1,10 @@
+x = float(input("enter the first number:"))
+y = float(iput("enter the second number:"))
+z = float(input("enter the last number:"))
+ if (x>y) and (x>z):
+largest = x
+ elif (y>x) and (y>z):
+largest = x
+ else:
+largest = z
+  print("The largest number is",largest)
