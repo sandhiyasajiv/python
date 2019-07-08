2e62acb0b0ee5353de79b510860de15f4b0cc9cc
sandhiyasajiv <52270651+sandhiyasajiv@users.noreply.github.com>
2019-07-08 11:28:44 +0530

    Create leapyearor_not.py

---
+++ leapyearor_not.py
@@ -0,0 +1,12 @@
+year = 200
+ if (year %4) ==0:
+  if(year %200) ==0:
+   if(year%500) ==0);
+     print("{0} is a leap year" . format(year))
+ else:
+     print("{0} is not a leap year".format(year))
+ else:
+     print("{0" is a leap year ".format(year))
+else:
+   print("{0} is not a leap year".format(year))
+
