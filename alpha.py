17917b5aed0d9a423ec4c4e7741d667997d3b109
sandhiyasajiv <52270651+sandhiyasajiv@users.noreply.github.com>
2019-07-15 15:28:10 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,11 @@
+x,y = map(str,input().split())
+m = 0
+if len(x)>len(y):
+	x,y=y,x
+a = 0
+while x<len(x):
+	  m += (ordy(a])-ord(a[x]))
+	a += 1
+for a in range(a,len(y)):
+	  m += ord(y[(a)-ord('a')+1]
+print(m)
