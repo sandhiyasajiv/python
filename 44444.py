9ea5cc9f70f3dd3dc590c682008f0a6f85e26da0
sandhiyasajiv <52270651+sandhiyasajiv@users.noreply.github.com>
2019-08-09 14:49:32 +0530

    Create 44444.py

---
+++ 44444.py
@@ -0,0 +1,9 @@
+s=int(input())
+l=list(map(int,input().split()))
+l1=[]
+for j in l:
+	r=bin(j)[2::]
+	l1.append(([s.count("j"),j]))
+l1.sort(reverse=True)
+for i in range(0,len(l1)):
+	print(l1[j][1]
